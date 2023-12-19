06d113cfb257586df5f4d7f3c00075e8e27110df
Pdawg11239 <pdawg11239linusfan@gmail.com>
2023-12-18 22:12:26 -0500

    Work on desktop navigation

---
+++ factoryos-10x-shell.Library/Services/Navigation/IDesktopNavigator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace factoryos_10x_shell.Library.Services.Navigation
+{
+    public enum DesktopPageType
+    {
+        RootContentDesktop,
+        RootContentLockScreen
+    }
+
+    public interface IDesktopNavigator
+    {
+        public void DesktopNavigate();
+
+        public Frame FrameContext { get; set; }
+    }
+}
---
+++ factoryos-10x-shell/Services/Navigation/DesktopNavigator.cs
@@ -0,0 +1,35 @@
+﻿using factoryos_10x_shell.Controls.DesktopControls;
+using factoryos_10x_shell.Controls.LockControls;
+using factoryos_10x_shell.Library.Services.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace factoryos_10x_shell.Services.Navigation
+{
+    internal class DesktopNavigator
+    {
+        private readonly IDesktopNavigator m_desktopNavigator;
+
+        public DesktopNavigator(IDesktopNavigator desktopNavigator)
+        {
+            m_desktopNavigator = desktopNavigator;
+        }
+
+        public Frame FrameContext { get; set; }
+
+        public void DesktopNavigate(DesktopPageType pageType)
+        {
+            Type navType = pageType switch
+            {
+                DesktopPageType.RootContentLockScreen => typeof(LockScreen),
+                DesktopPageType.RootContentDesktop => typeof(MainDesktop)
+            };
+
+            FrameContext.Navigate(navType);
+        }
+    }
+}
